708bf02d5d804bd12fc5036c4e2cc8831568c335
Alyssa Petersen <aptrsn11@gmail.com>
2022-09-19 11:55:52 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// require apps
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -16,6 +17,9 @@ app.use(
 );
 app.use(express.static("public"));
 
+//begin get requests
+
+// get html files
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -24,11 +28,13 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// get data from json database
 app.get("/api/notes", (req, res) => {
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   res.json(notes);
 });
 
+//begin post request
 app.post("/api/notes", (req, res) => {
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   let newNote = req.body;
@@ -40,24 +46,24 @@ app.post("/api/notes", (req, res) => {
   res.json(allNotes);
 });
 
+//begin delete function
 app.delete("/api/notes/:id", (req, res) => {
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   let deleteNote = req.params.id;
-  console.log('Id notes: ', deleteNote);
+  console.log("Id notes: ", deleteNote);
 
-  let updateNotes = []
-  notes.forEach(note => {
-    if(note.id != deleteNote){
-        updateNotes.push(note)
+  let updateNotes = [];
+  notes.forEach((note) => {
+    if (note.id != deleteNote) {
+      updateNotes.push(note);
     }
-  })
-
-  console.log('Deleted notes: ', updateNotes);
+  });
 
   fs.writeFileSync("./db/db.json", JSON.stringify(updateNotes));
   res.json(updateNotes);
 });
 
+// app listen
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
